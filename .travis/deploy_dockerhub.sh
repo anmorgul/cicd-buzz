8bdbbdaf6e0897221c3eec0e218c117e65c67ee8
anmorgul <anmorgul@gmail.com>
2018-02-08 20:32:05 +0200

    step7_8

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -e anmorgul@gmail.com -u anmorgul -p DoPa541fyv
 if [ "$TRAVIS_BRANCH" = "master" ]; then
     TAG="latest"
 else
     TAG="$TRAVIS_BRANCH"
 fi
+
 echo $TRAVIS_REPO_SLUG:$TAG
+
 docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
 
 docker push $TRAVIS_REPO_SLUG
